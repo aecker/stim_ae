60c01db06345f4e48e3a8f2ee9ba794dd13619e7
pberens <pberens@d1c95485-6871-e240-9df0-63fa95a8ea69>
2007-12-18 15:44:49 +0000

    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@668 d1c95485-6871-e240-9df0-63fa95a8ea69

--- exp/@MovingBarExperiment/netShowStimulus.m
+++ exp/@MovingBarExperiment/netShowStimulus.m
@@ -2,6 +2,8 @@ function [e,retInt32,retStruct,returned] = netShowStimulus(e,params)
 % Show stimulus.
 % AE & PhB 2007-10-09
 
+fprintf('condition: %d',getParam(e,'condition'));
+
 % show stimulus depending on which block we're in
 if getParam(e,'isMapping')
     [e,retInt32,retStruct,returned] = showFlickeringBar(e);
