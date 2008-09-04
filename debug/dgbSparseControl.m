3d60511310afa193e68c1e8c1d669c5100f25074
ahoenselaar <ahoenselaar@d1c95485-6871-e240-9df0-63fa95a8ea69>
2008-09-04 23:41:22 +0000

    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@1563 d1c95485-6871-e240-9df0-63fa95a8ea69

--- debug/dgbSparseControl.m
+++ debug/dgbSparseControl.m
@@ -6,25 +6,26 @@ warning off TrialBasedExperiment:netEndTrial
 warning off StimulationData:setTrialParam
 warning off StimulationData:setParam
 
-T = SparseCodingExperiment;
+T = SparseCodingControlExperiment;
 T = set(T,'debug',true);
 T = openWindow(T);
 
 constants.subject = 'DEBUG';
 constants.eyeControl = 0;
+constants.stimulusTime = 500;
 
 constants.bgColor = [64; 64; 64];
 constants.fixSpotColor = [255; 0; 0];
 constants.fixSpotLocation = [0; 0];
 constants.fixSpotSize = 10;
 
-constants.location = [250; 250];
+constants.location = [50; 50];
 constants.diskSize = 200;
 constants.fadeFactor = 1.3;
 
-constants.imagePath = '\\steinlach\berens\projects\sparsecoding\stimuli\stimuli';
+constants.imagePath = '/lab/users/philipp/sparsecoding/stimuli';
 constants.imageStats = [1,2,3];
-constants.modFunction = {'1'};
+constants.modFunction = '1';
 
 constants.rewardProb = 1;
 
@@ -32,6 +33,11 @@ constants.delayTime = 1000;
 constants.postStimulusTime = 250;
 constants.rewardAmount = 0;
 constants.date = datestr(now,'YYYY-mm-dd_HH-MM-SS');
+constants.joystickThreshold = 10000;
+constants.fixationRadius = 1000;
+constants.passive = true;
+constants.acquireFixation = 10;
+constants.allowSaccades = false;
 
 trials = struct;
 
@@ -46,7 +52,7 @@ for i = 1:20
     T = netSync(T,struct('counter',1));
     T = netInitTrial(T);
     
-    T = netAcquireFixation(T,struct);
+%    T = netAcquireFixation(T,struct);
     pause(0.5)
     
     T = netShowStimulus(T,struct);
