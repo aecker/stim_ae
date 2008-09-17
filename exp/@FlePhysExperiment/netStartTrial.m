88787db2b14bffeec508c1f529f9bbe980c05a3e
aecker <aecker@d1c95485-6871-e240-9df0-63fa95a8ea69>
2008-09-17 15:45:34 +0000

    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@1582 d1c95485-6871-e240-9df0-63fa95a8ea69

--- exp/@FlePhysExperiment/netStartTrial.m
+++ exp/@FlePhysExperiment/netStartTrial.m
@@ -5,5 +5,8 @@ function [e,retInt32,retStruct,returned] = netStartTrial(e,params)
 [e.TrialBasedExperiment,retInt32,retStruct,returned] = ...
     netStartTrial(e.TrialBasedExperiment,params);
 
+% Because stupid Matlab doesn't know virtual functions...
+e = initTrial(e);
+
 % return variable delayTime to LabView
 retStruct.delayTime = getParam(e,'delayTime');
