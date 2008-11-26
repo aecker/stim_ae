d9a4b12bdc56072bf5fd145cd92bd8c8262f27a3
ahoenselaar <ahoenselaar@d1c95485-6871-e240-9df0-63fa95a8ea69>
2008-11-26 19:40:52 +0000

    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@1829 d1c95485-6871-e240-9df0-63fa95a8ea69

--- rand/@AllImagesRandomization/getNextTrial.m
+++ rand/@AllImagesRandomization/getNextTrial.m
@@ -5,5 +5,5 @@ function [r,condIndex] = getNextTrial(r)
 %     error('All trials completed. LabView should have read out the ''mustStop'' return value of trialCompleted!')
 % end
 
-r.conditionIdx = ceil(rand(length(r.conditionPool)));
+r.conditionIdx = ceil(rand * length(r.conditionPool));
 condIndex = r.conditionPool(r.conditionIdx);
