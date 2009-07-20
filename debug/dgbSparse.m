4966a4764ce5395bb3655aafe2fcea67fdf47aff
aecker <aecker@d1c95485-6871-e240-9df0-63fa95a8ea69>
2009-07-20 13:47:40 +0000

    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@2345 d1c95485-6871-e240-9df0-63fa95a8ea69

--- debug/dgbSparse.m
+++ debug/dgbSparse.m
@@ -18,17 +18,28 @@ constants.fixSpotColor = [255; 0; 0];
 constants.fixSpotLocation = [0; 0];
 constants.fixSpotSize = 10;
 
-constants.location = [250; 250];
+constants.location = [-10; 40];
 constants.diskSize = 200;
-constants.fadeFactor = 1.3;
+constants.fadeFactor = 1.4;
 
-constants.imagePath = '\\steinlach\berens\projects\sparsecoding\stimuli\stimuli';
+constants.imageFile = '/kyb/agbs/aecker/lab/tmp/textures_400.mat';
+constants.imageNumber = 1:5;
 constants.imageStats = [1,2,3];
-constants.modFunction = {'1'};
+constants.modFunction = '1';
+
+constants.monitorSize = [41; 30];
+constants.monitorDistance = 109;
+constants.monitorCenter = [920; 600];
 
 constants.rewardProb = 1;
+constants.joystickThreshold = 100;
+constants.fixationRadius = 50;
+constants.passive = 1;
+constants.acquireFixation = 1;
+constants.allowSaccades = 0;
 
-constants.delayTime = 1000;
+constants.stimulusTime = 1000;
+constants.delayTime = 1250;
 constants.postStimulusTime = 250;
 constants.rewardAmount = 0;
 constants.date = datestr(now,'YYYY-mm-dd_HH-MM-SS');
@@ -46,12 +57,12 @@ for i = 1:20
     T = netSync(T,struct('counter',1));
     T = netInitTrial(T);
     
-    T = netAcquireFixation(T,struct);
+    T = netShowFixSpot(T,struct);
     pause(0.5)
     
     T = netShowStimulus(T,struct);
     
-    T = netTrialOutcome(T,struct('correctResponse',true));
+    T = netTrialOutcome(T,struct('correctResponse',true,'behaviorTimestamp',NaN));
     T = netEndTrial(T);
 
     pause(0.5)
