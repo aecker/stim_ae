3e65b5deca74dac28a035bc32e7af0a663d47ca0
aecker <aecker@d1c95485-6871-e240-9df0-63fa95a8ea69>
2011-05-27 14:08:22 +0000

    Changed meaning of delayTime parameter in FlashLagExperiment: it is now relative to the time of the flash instead of relative to the onset of the stimulus. This was necessary because the time of the flash can vary
    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@3718 d1c95485-6871-e240-9df0-63fa95a8ea69

--- exp/@FlashLagExperiment/netShowStimulus.m
+++ exp/@FlashLagExperiment/netShowStimulus.m
@@ -2,6 +2,14 @@ function [e,retInt32,retStruct,returned] = netShowStimulus(e,varargin)
 % Show stimulus.
 % AE 2007-10-17
 
+% as this is a non-blocking function call from LabView, we have to return it
+% manually
+retStruct.trialIndex = int32(getParam(e,'trialIndex'));
+retStruct.trialType = int32(getParam(e,'trialType'));
+retStruct.blockSize = int32(getParam(e,'blockSize'));
+retStruct.blockType = int32(getParam(e,'blockType'));
+tcpReturnFunctionCall(e,int32(0),retStruct);
+
 % some member variables..
 win     = get(e,'win');
 refresh = get(e,'refreshRate');
@@ -11,114 +19,25 @@ barSize          = getParam(e,'barSize');
 len              = getParam(e,'trajectoryLength');
 speed            = getParam(e,'speed');
 flashDuration    = getParam(e,'flashDuration');
-perceivedLag     = getParam(e,'perceivedLag');
-flashLoc         = getParam(e,'flashLocation'); % constant location for training
-flashLocMult     = getParam(e,'flashLocations'); % variable locations for experiment (used with expmode)
-randLocation     = getParam(e,'randLocation');
-noFlashZone      = getParam(e,'noFlashZone');
-lagProb          = getParam(e,'lagProb');
-expMode          = getParam(e,'expMode');       % expMode=true means we use the fixed flashOffsets
-offsetY          = getParam(e,'yOffset');
-flashOffset      = getParam(e,'flashOffset');   % constant offset for training  
-flashOffsets     = getParam(e,'flashOffsets');  % different offsets used for experiment
-offsetThreshold  = getParam(e,'offsetThreshold');
-trialType        = getParam(e,'trialType');
+xOffset          = getParam(e,'xOffset');
+yOffset          = getParam(e,'yOffset');
 trajectoryAngle  = getParam(e,'trajectoryAngle');
 trajectoryCenter = getParam(e,'trajectoryCenter');
-
-% response targets
-monitorCenter    = getParam(e,'monitorCenter');
-leftTarget       = monitorCenter + getParam(e,'leftTarget');
-rightTarget      = monitorCenter + getParam(e,'rightTarget');
+moveDir          = getParam(e,'moveDir');
+movingLoc        = getParam(e,'movingLoc');
+leftTarget       = getParam(e,'leftTarget');
+rightTarget      = getParam(e,'rightTarget');
 targetRadius     = getParam(e,'targetRadius');
 targetColor      = getParam(e,'targetColor');
-
-moveDir = rand(1) > getParam(e,'moveProb');
-e = setTrialParam(e,'moveDir',moveDir);
-e = setTrialParam(e,'stimulusTime',len / speed * 1000);
-
-% For training we use a fixed offset (flashOffset) set on the front panel. For
-% the experiment we use a fixed set of offsets (flashOffsets) defined in the
-% config file
-if expMode == FlashLagExperiment.TRAINING
-    lagDir = 2 * (rand(1) > lagProb) - 1;
-    flashOffset = lagDir * flashOffset;
-
-else % if expMode == FlashLagExperiment.EXPERIMENT
-    if trialType == ReportPerceptRandomization.PROBE
-        offsets = find(abs(flashOffsets) <= offsetThreshold);
-    else % if trialType == ReportPerceptRandomization.REGULAR_[NO_]REWARD
-        offsets = find(abs(flashOffsets) > offsetThreshold);
-    end
-    ndx = offsets(ceil(rand(1) * length(offsets)));
-    flashOffset = flashOffsets(ndx);
-end
-e = setTrialParam(e,'flashOffset',flashOffset);
-
-% The response should indicate the location of the flashed bar relative to the 
-% moving bar
-if flashOffset < 0 && moveDir == FlashLagExperiment.MOTION_LEFT || ...
-        flashOffset >= 0 && moveDir == FlashLagExperiment.MOTION_RIGHT
-    retStruct.correctResponse = int32(TrialBasedExperiment.RIGHT_JOYSTICK);
-    disp('correct response: right')
-else
-    retStruct.correctResponse = int32(TrialBasedExperiment.LEFT_JOYSTICK);
-    disp('correct response: left')
-end
-retStruct.trialIndex = int32(getParam(e,'trialIndex'));
-retStruct.trialType = int32(getParam(e,'trialType'));
-retStruct.blockSize = int32(getParam(e,'blockSize'));
-retStruct.blockType = int32(getParam(e,'blockType'));
-
-% inform state system of target locations
-retStruct.leftTarget = leftTarget;
-retStruct.rightTarget = rightTarget;
-
-% as this is a non-blocking function call from LabView, we have to return it
-% manually
-tcpReturnFunctionCall(e,int32(0),retStruct);
 
 % determine starting position
 angle = (trajectoryAngle + 180 * moveDir) / 180 * pi;
 startPos = trajectoryCenter - len/2 * [cos(angle); -sin(angle)];
 
-% (1) We want the offset to be relative to the average location of the moving
-%     bar. Since the exact flash location is calculated when the flash starts
-%     we need to adjust the offset by the expected number of pixels the bar
-%     moves during the flash. This is #(frames-1)*flashDuration
-% (2) In order to make the offsets appear approximately symmetric during
-%     training, we compensate for the perceived lag.
-% (3) Positive offset means the flashed bar is ahead of the moving bar
-offsetMove = (flashDuration - 1) * speed / refresh / 2;
-offsetX = flashOffset + offsetMove + perceivedLag;
-
 % AE 2010-10-23: Changed the vertical offset to be a parameter
-% offsetY = 1.2 * barSize(2);
-offset = offsetX * [cos(angle); -sin(angle)] ...
-       + offsetY * [sin(mod(angle,pi)); cos(mod(angle,pi))];
-
-% in training, we allow randomized and fixed locations. in the experiment,
-% we give it a number of locations at which the flash may be shown
-
-if expMode == FlashLagExperiment.TRAINING
-    if randLocation
-        movingLoc = noFlashZone + abs(flashOffset) * (flashOffset < 0) ...
-            + rand(1) * ...
-                (len - abs(flashOffset) - 2 * (noFlashZone + offsetMove) - perceivedLag);
-        flashLoc = movingLoc + offsetX;
-    else
-        movingLoc = flashLoc - offsetX;
-    end
-else
-    ndx = ceil(rand*length(flashLocMult));
-    flashLoc = flashLocMult(ndx);
-    movingLoc = flashLoc - offsetX;
-end
-e = setTrialParam(e,'flashLocation',flashLoc);
-e = setTrialParam(e,'movingLocation',movingLoc);
-
-fprintf('flashLocation: %d | movingLocation: %d | offset: %d\n', ....
-    round(flashLoc),round(movingLoc),flashOffset)
+% yOffset = 1.2 * barSize(2);
+offset = xOffset * [cos(angle); -sin(angle)] ...
+       + yOffset * [sin(mod(angle,pi)); cos(mod(angle,pi))];
 
 % put start time
 firstTrial = true;
@@ -166,7 +85,7 @@ while s(i) < len
         firstTrial = false;
     end
 
-    i = i+1;
+    i = i + 1;
     
     % compute next position
     s(i) = (getLastSwap(e) - startTime + 1 / refresh) * speed;
--- exp/@FlashLagExperiment/netStartTrial.m
+++ exp/@FlashLagExperiment/netStartTrial.m
@@ -21,13 +21,105 @@ e = set(e,'randomization',r);
 
 % target information needs to go back to LabView
 targetLocation = getParam(e,'targetLocation');
-leftTarget = targetLocation;
-rightTarget = targetLocation .* [-1; 1];
+monitorCenter = getParam(e,'monitorCenter');
+leftTarget = monitorCenter + targetLocation;
+rightTarget = monitorCenter + targetLocation .* [-1; 1];
 e = setTrialParam(e,'leftTarget',leftTarget);
 e = setTrialParam(e,'rightTarget',rightTarget);
 retStruct.leftTarget = leftTarget;
 retStruct.rightTarget = rightTarget;
 retStruct.targetRadius = getParam(e,'targetRadius');
+
+len             = getParam(e,'trajectoryLength');
+speed           = getParam(e,'speed');
+flashLoc        = getParam(e,'flashLocation'); % constant location for training
+flashLocMult    = getParam(e,'flashLocations'); % variable locations for experiment (used with expmode)
+randLocation    = getParam(e,'randLocation');
+noFlashZone     = getParam(e,'noFlashZone');
+flashDuration   = getParam(e,'flashDuration');
+perceivedLag    = getParam(e,'perceivedLag');
+lagProb         = getParam(e,'lagProb');
+expMode         = getParam(e,'expMode');       % expMode=true means we use the fixed flashOffsets
+flashOffset     = getParam(e,'flashOffset');   % constant offset for training  
+flashOffsets    = getParam(e,'flashOffsets');  % different offsets used for experiment
+offsetThreshold = getParam(e,'offsetThreshold');
+trialType       = getParam(e,'trialType');
+refresh         = get(e,'refreshRate');
+
+% pick random direction of motion
+moveDir = rand(1) > getParam(e,'moveProb');
+e = setTrialParam(e,'moveDir',moveDir);
+e = setTrialParam(e,'stimulusTime',len / speed * 1000);
+
+% For training we use a fixed offset (flashOffset) set on the front panel. For
+% the experiment we use a fixed set of offsets (flashOffsets) defined in the
+% config file
+if expMode == FlashLagExperiment.TRAINING
+    lagDir = 2 * (rand(1) > lagProb) - 1;
+    flashOffset = lagDir * flashOffset;
+
+else % if expMode == FlashLagExperiment.EXPERIMENT
+    if trialType == ReportPerceptRandomization.PROBE
+        offsets = find(abs(flashOffsets) <= offsetThreshold);
+    else % if trialType == ReportPerceptRandomization.REGULAR_[NO_]REWARD
+        offsets = find(abs(flashOffsets) > offsetThreshold);
+    end
+    ndx = offsets(ceil(rand(1) * length(offsets)));
+    flashOffset = flashOffsets(ndx);
+end
+e = setTrialParam(e,'flashOffset',flashOffset);
+
+% The response should indicate the location of the flashed bar relative to the 
+% moving bar
+if flashOffset < 0 && moveDir == FlashLagExperiment.MOTION_LEFT || ...
+        flashOffset >= 0 && moveDir == FlashLagExperiment.MOTION_RIGHT
+    retStruct.correctResponse = int32(TrialBasedExperiment.RIGHT_JOYSTICK);
+    disp('correct response: right')
+else
+    retStruct.correctResponse = int32(TrialBasedExperiment.LEFT_JOYSTICK);
+    disp('correct response: left')
+end
+
+% (1) We want the offset to be relative to the average location of the moving
+%     bar. Since the exact flash location is calculated when the flash starts
+%     we need to adjust the offset by the expected number of pixels the bar
+%     moves during the flash. This is #(frames-1)*flashDuration
+% (2) In order to make the offsets appear approximately symmetric during
+%     training, we compensate for the perceived lag.
+% (3) Positive offset means the flashed bar is ahead of the moving bar
+offsetMove = (flashDuration - 1) * speed / refresh / 2;
+xOffset = flashOffset + offsetMove + perceivedLag;
+e = setTrialParam(e,'xOffset',xOffset);
+
+% in training, we allow randomized and fixed locations. in the experiment,
+% we give it a number of locations at which the flash may be shown
+if expMode == FlashLagExperiment.TRAINING
+    if randLocation
+        movingLoc = noFlashZone + abs(flashOffset) * (flashOffset < 0) ...
+            + rand(1) * ...
+                (len - abs(flashOffset) - 2 * (noFlashZone + offsetMove) - perceivedLag);
+        flashLoc = movingLoc + xOffset;
+    else
+        movingLoc = flashLoc - xOffset;
+    end
+else
+    ndx = ceil(rand*length(flashLocMult));
+    flashLoc = flashLocMult(ndx);
+    movingLoc = flashLoc - xOffset;
+end
+e = setTrialParam(e,'flashLocation',flashLoc);
+e = setTrialParam(e,'movingLocation',movingLoc);
+
+fprintf('flashLocation: %d | movingLocation: %d | offset: %d\n', ....
+    round(flashLoc),round(movingLoc),flashOffset)
+
+% determine delay time (is specified relative to time of flash)
+relDelayTime = getParam(e,'delayTime');
+flashTime = movingLoc / speed * 1000;
+delayTime = flashTime + relDelayTime;
+e = setTrialParam(e,'relDelayTime',relDelayTime);
+e = setTrialParam(e,'delayTime',delayTime);
+retStruct.delayTime = delayTime;
 
 % Punish according to user specification
 inCorrectResponse = ~(getParam(e,'correctResponse'));
@@ -47,5 +139,5 @@ switch params.punishType
             punishTime = params.punishTime;
         end
 end
-
 retStruct.punishTime = int32(punishTime);
+
