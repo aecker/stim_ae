05947649666d4e5f8bc29ab6634c32faccc418cb
mani <mani@d1c95485-6871-e240-9df0-63fa95a8ea69>
2011-12-08 23:59:10 +0000

    Mani modified the rewardAmount variable to accommodate the new acquisition system.
    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@4244 d1c95485-6871-e240-9df0-63fa95a8ea69

--- exp/@DotMappingExperiment/netTrialOutcome.m
+++ exp/@DotMappingExperiment/netTrialOutcome.m
@@ -5,12 +5,18 @@ function [e,retInt32,retStruct,returned] = netTrialOutcome(e,params)
 [e.TrialBasedExperiment,retInt32,retStruct,returned] = ...
     netTrialOutcome(e.TrialBasedExperiment,params);
 
+%TODO: Remove try catch block when transitioned to new state chart
+try
+
 isReward = 1;
 if getParam(e,'randReward')
     rewardAmount = isReward * rand(1) * getParam(e,'rewardAmount');
 else
     rewardAmount = isReward * getParam(e,'rewardAmount');
 end
-
 e = setTrialParam(e,'rewardAmount',rewardAmount);
 retStruct.reward = int32(rewardAmount);
+
+catch
+end
+
