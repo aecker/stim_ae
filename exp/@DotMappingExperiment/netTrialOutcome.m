4ab606def31838b0d5fdd4df3c014b9988b6702f
mani <mani@d1c95485-6871-e240-9df0-63fa95a8ea69>
2012-10-26 23:17:19 +0000

    Added netStartSession.m
    git-svn-id: svn://at-lab.neusc.bcm.tmc.edu/alex/stimulation@4618 d1c95485-6871-e240-9df0-63fa95a8ea69

--- exp/@DotMappingExperiment/netTrialOutcome.m
+++ exp/@DotMappingExperiment/netTrialOutcome.m
@@ -7,16 +7,16 @@ function [e,retInt32,retStruct,returned] = netTrialOutcome(e,params)
 
 %TODO: Remove try catch block when transitioned to new state chart
 try
-
-isReward = 1;
-if getParam(e,'randReward')
-    rewardAmount = isReward * rand(1) * getParam(e,'rewardAmount');
-else
-    rewardAmount = isReward * getParam(e,'rewardAmount');
-end
-e = setTrialParam(e,'rewardAmount',rewardAmount);
-retStruct.reward = int32(rewardAmount);
-
+    
+    isReward = 1;
+    if getParam(e,'randReward')
+        rewardAmount = isReward * rand(1) * getParam(e,'rewardAmount');
+    else
+        rewardAmount = isReward * getParam(e,'rewardAmount');
+    end
+    e = setTrialParam(e,'rewardAmount',rewardAmount);
+    retStruct.reward = int32(rewardAmount);
+    
 catch
 end
 
