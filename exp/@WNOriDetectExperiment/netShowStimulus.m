8e068552eb663dcff9a75fb1f3d60b4f84cb2d5b
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-11-27 15:07:42 +0100

    added some paramaters to storage

--- exp/@WNOriDetectExperiment/netShowStimulus.m
+++ exp/@WNOriDetectExperiment/netShowStimulus.m
@@ -118,6 +118,8 @@ e = setTrialParam(e, 'orientationsAll', orientations(1 : i));
 e = setTrialParam(e, 'nFramesPre', numel(oriPre));
 e = setTrialParam(e, 'nFramesCoh', numel(oriCoh));
 e = setTrialParam(e, 'nFramesPost', numel(oriPost));
+e = setTrialParam(e, 'delayTime', delayTime);
+e = setTrialParam(e, 'catchTrial', catchTrial);
 
 % return values
 retInt32 = int32(0);
