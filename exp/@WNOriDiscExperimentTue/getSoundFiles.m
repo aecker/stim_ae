7049fd20543079dd4df0d0c88b7882c5f87ccc54
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-11-30 11:05:37 +0100

    Added functions for sound and events

---
+++ exp/@WNOriDiscExperimentTue/getSoundFiles.m
@@ -0,0 +1,13 @@
+function soundFiles = getSoundFiles(e)
+% Return structure containing file names for each sound.
+% AE 2007-10-05
+
+soundFiles.startTrial = 'startTrialDefault';
+soundFiles.eyeAbort = 'incorrectResponse';
+soundFiles.noResponse = 'eyeAbortDefault';
+soundFiles.leverAbort = 'leverAbortDefault';
+soundFiles.buttonAbort = 'leverAbortDefault';
+soundFiles.reward = 'correctResponse';
+soundFiles.prematureAbort = 'prematureAbortDefault';
+soundFiles.correctResponse = 'correctResponse';
+soundFiles.incorrectResponse = 'eyeAbortDefault';
