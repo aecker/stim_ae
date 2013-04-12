d9ee07be286ab0caed195b0e3ef94c606a0362ce
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-04-12 02:57:13 +0200

    Couple of improvements to WNOriDetTue, which I forgot to commit a while ago

--- exp/@WNOriDetectExperimentTue/getEventTypes.m
+++ exp/@WNOriDetectExperimentTue/getEventTypes.m
@@ -1,12 +1,12 @@
-function [types,sites] = getEventTypes(e)
+function [types, sites] = getEventTypes(e)
 % Here we add some custom events.
 % AE 2011-09-19
 
 % first get all events defined by the parent class
-[types,sites] = getEventTypes(e.WNOriDetectExperiment);
+[types, sites] = getEventTypes(e.WNOriDetectExperiment);
 
 % and now add our own one for eye tracker start
-types = [types; {'eyeTrackerStart'; 'buttonAbort'}];
+types = [types; {'eyeTrackerStart'; 'buttonAbort'; 'feedback'}];
 
 % Site that triggers the above events (0 = Mac/Matlab; 1 = PC/LabView)
-sites = [sites, 0, 0];
+sites = [sites, 0, 0, 0];
